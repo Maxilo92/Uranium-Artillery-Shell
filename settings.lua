7a23663c1933d6c01cfcf63a93252e1439ce9869
Maximilian Priesnitz <maximilian.priesnitz@example.com>
2025-12-18 18:29:37 +0100

    Fix crash due to missing settings and update changelog

--- settings.lua
+++ settings.lua
@@ -5,5 +5,38 @@ data:extend({
         setting_type = "runtime-global",
         default_value = true,
         order = "a"
+    },
+    {
+        type = "int-setting",
+        name = "uranium-artillery-radius",
+        setting_type = "startup",
+        default_value = 15,
+        minimum_value = 1,
+        maximum_value = 100,
+        order = "b"
+    },
+    {
+        type = "double-setting",
+        name = "uranium-artillery-damage",
+        setting_type = "startup",
+        default_value = 2000,
+        minimum_value = 1,
+        order = "c"
+    },
+    {
+        type = "double-setting",
+        name = "uranium-radiation-damage-percent",
+        setting_type = "runtime-global",
+        default_value = 5,
+        minimum_value = 0,
+        maximum_value = 100,
+        order = "d"
+    },
+    {
+        type = "bool-setting",
+        name = "uranium-player-damage-enabled",
+        setting_type = "runtime-global",
+        default_value = true,
+        order = "e"
     }
 })
